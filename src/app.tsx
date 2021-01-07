4868d535c6ee70b6a066f1573db7a8dc68f6e51f
Qs-F <qsf@de-liker.com>
2021-01-08 00:26:35 +0900

    Add sample src

---
+++ src/app.tsx
@@ -0,0 +1,11 @@
+/** @jsx jsx */
+import { jsx } from '@emotion/react'
+import { FC, Fragment } from 'react'
+
+export const App: FC = () => {
+  return (
+    <Fragment>
+      <p>Hello</p>
+    </Fragment>
+  )
+}
