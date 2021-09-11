74b96d16be9f4d14afb193caf1bd10dd9927c60f
Qs-F <qsf@de-liker.com>
2021-09-12 04:51:11 +0900

    Fix to use use-xf in example

--- src/app.tsx
+++ src/app.tsx
@@ -1,9 +1,11 @@
 import { FC, Fragment } from 'react'
+import { useXf } from '@creatorqsf/use-xf'
 
 export const App: FC = () => {
+  const [state, setState] = useXf((n: number): string => n.toString(), 0)
   return (
     <Fragment>
-      <p>Hello</p>
+      <p onClick={() => setState(10)}>{state}</p>
     </Fragment>
   )
 }
