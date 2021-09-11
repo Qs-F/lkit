6d7f64567a0b747a216866a7deba0d27ab4fcfd2
Qs-F <qsf@de-liker.com>
2021-09-12 04:55:53 +0900

    Revert "Fix to use use-xf in example"
    This reverts commit 74b96d16be9f4d14afb193caf1bd10dd9927c60f.

--- src/app.tsx
+++ src/app.tsx
@@ -1,11 +1,9 @@
 import { FC, Fragment } from 'react'
-import { useXf } from '@creatorqsf/use-xf'
 
 export const App: FC = () => {
-  const [state, setState] = useXf((n: number): string => n.toString(), 0)
   return (
     <Fragment>
-      <p onClick={() => setState(10)}>{state}</p>
+      <p>Hello</p>
     </Fragment>
   )
 }
